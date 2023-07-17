ef5532d911e04535bc592a19526312e0f56bd665
Cristian Zantedeschi <c.zantedeschi@gmail.com>
2023-07-17 08:43:45 +0200

    c

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def split_pdf_to_text(pdf_path, output_folder, pages_per_chunk):
             start_page = i * pages_per_chunk
             end_page = start_page + pages_per_chunk if start_page + pages_per_chunk < num_pages else num_pages
 
-            chunk_filename = f"chunk_{i + 1}.txt"
+            chunk_filename = output_folder +f"_{i + 1}.txt"
             chunk_path = os.path.join(output_folder, chunk_filename)
 
             with open(chunk_path, 'w') as chunk_file:
@@ -31,7 +31,11 @@ def split_pdf_to_text(pdf_path, output_folder, pages_per_chunk):
 
 
 # Usage example
-pdf_path = 'kurs-samoleczenia.pdf'
-output_folder = 'kurs-samoleczenia'
-pages_per_chunk = 10  # Specify the desired number of pages per chunk
+# pdf_path = 'kurs-samoleczenia.pdf'
+# output_folder = 'kurs-samoleczenia'
+
+pdf_path = 'Benjamin Graham - Inteligentny inwestor.pdf'
+output_folder = 'Benjamin Graham - Inteligentny inwestor'
+
+pages_per_chunk = 5  # Specify the desired number of pages per chunk
 split_pdf_to_text(pdf_path, output_folder, pages_per_chunk)
